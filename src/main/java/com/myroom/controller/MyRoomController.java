9f20505928b27461c2d9c4a55879776c77bf5d2f
prashanth <prashanthmp009@gmail.com>
2022-03-12 23:37:24 +0530

    cors added

--- src/main/java/com/myroom/controller/MyRoomController.java
+++ src/main/java/com/myroom/controller/MyRoomController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import com.myroom.model.ProductDetails;
 import com.myroom.model.Users;
 import com.myroom.repo.UsersRepo;
 import com.myroom.service.MyRoomService;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class MyRoomController {
 @Autowired
