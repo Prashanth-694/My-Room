7701b22fd353439b853ca71d3b642d8a5bece789
prashanth <prashanthmp009@gmail.com>
2022-03-13 00:45:42 +0530

    change cors

--- src/main/java/com/myroom/controller/UsersController.java
+++ src/main/java/com/myroom/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.myroom.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.myroom.model.Users;
 import com.myroom.service.UsersService;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class UsersController {
 
