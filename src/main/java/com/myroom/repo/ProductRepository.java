aff4ae87cabc18ed46ae3a353001d4167449785f
prashanth <prashanthmp009@gmail.com>
2022-03-13 01:14:18 +0530

    change cors

--- src/main/java/com/myroom/repo/ProductRepository.java
+++ src/main/java/com/myroom/repo/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.myroom.repo;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.myroom.model.ProductDetails;
 
 public interface ProductRepository extends MongoRepository<ProductDetails, Integer> {
-
+List<ProductDetails> findByUserId(int userId);
 }
--- src/main/java/com/myroom/service/MyRoomService.java
+++ src/main/java/com/myroom/service/MyRoomService.java
@@ -1,5 +1,6 @@
 package com.myroom.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,18 +9,17 @@ import com.myroom.repo.ProductRepository;
 
 @Service
 public class MyRoomService {
-@Autowired
-ProductRepository productRepository;
+	@Autowired
+	ProductRepository productRepository;
 
-public ProductDetails insertProducts(ProductDetails productDetails)
-{
-	System.out.println("inside ProductDetails Sercvice insert");
-	return productRepository.save(productDetails);	
-}
+	public ProductDetails insertProducts(ProductDetails productDetails) {
+		System.out.println("inside ProductDetails Sercvice insert");
+		return productRepository.save(productDetails);
+	}
 
-public void fetchProductDetailsById(int userId) {
-	System.out.println("Requested "+userId);
-	productRepository.findById(userId);
-}
+	public List<ProductDetails> fetchProductDetailsById(int userId) {
+		System.out.println("Requested " + userId);
+		return productRepository.findByUserId(userId);
+	}
 
 }
